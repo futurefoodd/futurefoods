6dac272ba8e7c5bdcc3fbdac93526ac455825e89
Azrar <azrar2277@gmail.com>
2025-09-22 14:59:18 +0800

    feat: Added 'Tour' module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { DieteticsComponent } from './pages/dietetics/dietetics.component';
+import { TourComponent } from './pages/tour/tour.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'product-detail/:id', component: ProductDetailComponent },
   { path: 'dietetics', component: DieteticsComponent },
+  { path: 'tour', component: TourComponent },
   // { path: 'cart', component: CartComponent },
   // { path: 'checkout', component: CheckoutComponent },
   { path: '**', redirectTo: '' }
